8e1ef1b3df74722a5a6f74b56f12fd1d779f4a2f
Somyot <somyot.srihanon@gmail.com>
2018-02-20 18:52:53 +0700

    src

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { BarcodeScanner ,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  scanData : {};
+  encodeData : string ;
+  encodedData : {} ;
+  options :BarcodeScannerOptions;
 
+  constructor(public navCtrl: NavController, public navParams: NavParams,private barcodeScanner: BarcodeScanner) {
+  }  
+  
+  encodeText(){
+    this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE,this.encodeData).then((encodedData) => {
+
+        console.log(encodedData);
+        this.encodedData = encodedData;
+
+    }, (err) => {
+        console.log("เกิดข้อผิดพลาด : " + err);
+    });                 
+}
+
+scan(){
+  this.options = {
+      prompt : "สแกนรหัส QR Code ของคุณ "
   }
+  this.barcodeScanner.scan(this.options).then((barcodeData) => {
+
+      console.log(barcodeData);
+      this.scanData = barcodeData;
+  }, (err) => {
+      console.log("เกิดข้อผิดพลาด : " + err);
+  });         
+}    
 
 }
